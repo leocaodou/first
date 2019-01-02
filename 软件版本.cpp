42b11fcba4328bc4c0c8369a0ec061b617a4ed81
leocaodou <1210579369@qq.com>
2019-01-02 22:36:39 +0800

    lalala

---
+++ 软件版本.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,a,b,c,d,e,f;
+	printf("input number of test:\n"); 
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("input the first group version No.:(main sub revision)\n");
+		scanf("%d%d%d",&a,&b,&c);
+		printf("input  thesecond group version No.:(main sub revision)\n");
+		scanf("%d%d%d",&d,&e,&f);
+		printf("Output:\n");
+		if(a!=d)
+			if(a>d)
+			printf("First\n");
+			else
+			printf("Second\n");
+		else if(b!=e)
+				if(b>e)
+					printf("First\n");
+				else
+					printf("Second\n");
+			else if(c!=f)
+					if(c>f)
+						printf("First\n");
+					else
+						printf("Second\n");
+			else
+				printf("Same\n");
+	}
+	return 0;
+} 
