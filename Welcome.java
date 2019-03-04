db3757b3fa13995da4ed0a73537639eddb8cd2b4
leocaodou <1210579369@qq.com>
2019-03-04 22:33:18 +0800

    Learn java

--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class Welcome
 {
-	public static void main(String[] args){
-	System.out.println((9.0/5));
+	public static void main(String[] args)
+	{
+		Scanner input =new Scanner(System.in);
+		int y;
+		y = input.nextInt();
+		System.out.println(6+6);
 	}
 }
