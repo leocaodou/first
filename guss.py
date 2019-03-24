cf7b1b833e6884b0d45df60b7f16f8e8f28b11d2
leocaodou <1210579369@qq.com>
2019-03-24 19:54:13 +0800

    ttt

---
+++ guss.py
@@ -0,0 +1,12 @@
+i = 1
+while True :
+	a = 5
+	x = eval(input("请猜一个1~9的数字\n"))
+	if a > x :
+		print("遗憾，太小了")
+	elif a < x :
+		print("遗憾，太大了")
+	else :
+		print("预计{}次，你猜中了！".format(i))
+		break
+	i = i + 1
