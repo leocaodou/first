1bf1a032e3d0636058c1cbb3946edb0e267b8c5a
leocaodou <1210579369@qq.com>
2019-03-25 23:37:39 +0800

    tt

--- test.cpp
+++ test.cpp
@@ -1,12 +1,31 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+struct Stu
+{
+	int score;
+	char id[20];
+	char *name;
+	Stu()
+	{
+		score = 0;
+		name = NULL;
+		id[0] = '\0';
+	}
+	void setname(char *n)
+	{
+		name = (char*) malloc (sizeof(char) * (strlen(n) + 1) );
+		strcpy(name, n);
+	}
+	~Stu()
+	{
+		free(name);
+	}
+};
 int main()
 {
-	char *name="aaa";
-	char *a=name;
-	name=(char *)malloc(sizeof(char)*(strlen("aaa")+1));
-	strcpy(name,"bbb");
-	printf("%c,%s",*a,*name);
+	Stu lkh;
+	lkh.setname("liukehong");
+	printf("%s",lkh.name);
 	return 0;
 } 
