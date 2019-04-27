85c4edf8373477fdfdd0bc721e2cacbff9112dfb
leocaodou <1210579369@qq.com>
2019-04-27 23:38:36 +0800

    tt

--- Distances_to_Zero.cpp
+++ Distances_to_Zero.cpp
@@ -3,36 +3,47 @@
 using namespace std;
 int main()
 {
-	int n,i,k = -1;
+	int n,i,k = 1;
 	cin >> n;
 	int a[n];
+	int y = 0;
 	for(i = 0;i < n; i++)
 	{
 		cin >> a[i];
-		if(a[i] != 0 )
-		{
-			a[i] = k;
-			k++;
-		}
-		else
-			k = 1;
+		if(y == 1)
+			if(a[i] != 0 )
+			{
+				a[i] = k;
+				k++;
+			}
+			else
+				k = 1;
+		if(y == 0)
+			if(a[i] == 0)
+				y = 1;
+			else
+				a[i] = -1;
+			
 	}
-	int y = 0;
+	y = 0;
 	for(i = n -1; i >= 0;i--)
 	{
-		if(y == 0)
-			continue;
 		if(a[i] == 0)
 		{
 			y = 1;
 			k = 1;
 		}
-		else
-		{
-			if(k < a[i] || a[i] == -1)
-				a[i] = k;
-			k++;
-		}
+		if(y == 1)
+			if(a[i] == 0)
+			{
+				k = 1;
+			}
+			else
+			{
+				if(k < a[i] || a[i] == -1)
+					a[i] = k;
+				k++;
+			}
 	}
 	for(i = 0;i < n; i++)
 	{
