0b50f624275a74c803f629622976289332f9182b
leocaodou <1210579369@qq.com>
2019-04-28 23:58:21 +0800

    ti

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
-import numpy as np
-import matplotlib.pyplot as plt
-x = np.array([1,2,3,4,5,6,7,8])
-y = np.array([3,5,7,6,2,6,10,15])
-plt.plot(x,y,'r')# 折线 1 x 2 y 3 color
-plt.plot(x,y,'g',lw=10)# 4 line w
-# 折线 饼状 柱状
-x = np.array([1,2,3,4,5,6,7,8])
-y = np.array([13,25,17,36,21,16,10,15])
-plt.bar(x,y,0.2,alpha=1,color='b')# 5 color 4 透明度 3 0.9
-plt.show()
+import string,random
+password=[]
+resource=string.digits+string.ascii_letters
+for x in range(10):
+    password=[]
+    for number in range(8):
+        i=random.randint(0,len(resource)-1)
+        password.append(resource[i])
+    else:
+        print("".join(password))
