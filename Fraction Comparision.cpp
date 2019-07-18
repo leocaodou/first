1fb9847484a566a23843a9f666b396ae95ea03ec
leocaodou <1210579369@qq.com>
2019-07-18 22:42:13 +0800

    ttt

---
+++ Fraction Comparision.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	long long a,b,x,y;
+	
+}
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-def calcu_sub_str_num(mom_str,sun_str):
-	count = 0
-	for i in range(len(mom_str)-1):
-		if mom_str[i:i+len(sun_str)] == sun_str:
-			count+=1
-	return count
-mom_str = "lll"
-sun_str = input()
-print('%d'%calcu_sub_str_num(mom_str,sun_str))
+import sys 
+for line in sys.stdin:
+	a = line.split()
+	if eval(a[0])/eval(a[1]) > eval(a[2])/eval(a[3]) :
+		print(">")
+	elif eval(a[0])/eval(a[1]) == eval(a[2])/eval(a[3]):
+		print("=")
+	else:
+		print("<")
