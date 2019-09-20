5adde19796ae85d0646fe2d1ff1419fc14775abc
leocaodou <1210579369@qq.com>
2019-09-21 00:06:57 +0800

    tt

---
+++ test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class test{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		double a,b,c,d;
+		a = in.nextDouble();
+		b = in.nextDouble();
+		c = in.nextDouble();
+		b = b / 1200;
+		d = a * Math.pow((1 + b),(c * 12));
+		System.out.println(d);
+	}
+}
