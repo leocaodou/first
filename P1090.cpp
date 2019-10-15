7c8246aa6127a2c663fb4f56c6bdada4c54019fe
leocaodou <1210579369@qq.com>
2019-10-15 23:55:53 +0800

    tt

---
+++ P1090.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	for(int i = 0 ;i < n;i++)
+	{
+			
+	}
+}
